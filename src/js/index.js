1554a0b879e47bd0fe4225d2e8dd21ee1dbe3844
alex-suschin <alex_suschin@mail.ru>
2020-07-06 17:43:00 +0300

    top-wrap1

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,29 @@ import "./import/jquery.formstyler.min";
 
 $(function() {
 
+    jQuery("a.scrollto").click(function() {
+        elementClick = jQuery(this).attr("href")
+        destination = jQuery(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 700);
+        return false;
+    });
+
+    let phones = [
+        { 'mask': '+7 \\ \\ ###-###-##-##' }
+    ];
+
+    // $('input[type=tel]').inputmask({
+    //     mask: phones,
+    //     greedy: false,
+    //     definitions: {
+    //         '#': {
+    //             validator: '[0-9]',
+    //             cardinality: 1
+    //         }
+    //     }
+    // });
+
+
     setTimeout(function() {
         $('select').styler();
     }, 100)
