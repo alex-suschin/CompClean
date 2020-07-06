0e597596f50131487714c963c3dcd9bf92e2a73f
alex-suschin <alex_suschin@mail.ru>
2020-07-06 16:48:37 +0300

    header process

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
+import $ from "jquery";
 import "./import/modules";
+import "./import/jquery.formstyler.min";
+
+$(function() {
+
+    setTimeout(function() {
+        $('select').styler();
+    }, 100)
+
+});
