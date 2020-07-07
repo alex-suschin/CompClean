b79a16ab0d356be7d159aca0a5a8c47312abbd9f
alex-suschin <alex_suschin@mail.ru>
2020-07-07 12:52:21 +0300

    pre-finish-desk

--- src/js/index.js
+++ src/js/index.js
@@ -23,6 +23,33 @@ $(function() {
     //     });
     // });
 
+    ymaps.ready(function() {
+        var myMap = new ymaps.Map('map', {
+                center: [51.675628, 39.201276],
+                zoom: 17,
+                scrollZoom: false,
+                controls: ['zoomControl']
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+
+            myPlacemark = new ymaps.Placemark([51.675578, 39.203287], {
+                hintContent: 'Россия, Воронеж ул. Кольцовская 9а, оф. 626',
+                balloonContent: 'Россия, Воронеж ул. Кольцовская 9а, оф. 626'
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: './img/svg/pin.svg',
+                iconImageSize: [44, 60],
+                cursor: 'pointer',
+                iconImageOffset: [-30, -120],
+                balloonclose: true
+            });
+        myMap.behaviors.disable('scrollZoom');
+        myMap.geoObjects
+            .add(myPlacemark)
+
+    });
+
     jQuery("a.scrollto").click(function() {
         elementClick = jQuery(this).attr("href")
         destination = jQuery(elementClick).offset().top;
